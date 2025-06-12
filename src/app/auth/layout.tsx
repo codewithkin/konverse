ced501fdef0abcd6701379f4f72d94d269a4a31f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-06-12 11:04:02 +0200

    feat: enhance layout styling for improved responsiveness and spacing

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -9,17 +9,17 @@ export const metadata: Metadata = {
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
-        <main className="min-h-screen grid grid-rows-2 relative">
+        <main className="min-h-screen grid grid-rows-2 relative items-center justify-center md:items-stretch md:justify-normal text-center md:text-left">
             <article className="h-full bg-gradient-to-tr from-white to-sky-400"></article>
-            <article className="h-full flex flex-col justify-between p-4 md:p-16">
+            <article className="h-full flex flex-col justify-between p-4 md:p-16 gap-4 md:gap-0">
                 {/* Copy */}
-                <article className="flex flex-col">
+                <article className="flex flex-col gap-2 md:gap-0">
                     <h2 className="font-semibold text-2xl">Your WhatsApp storefront, powered by <span className="font-bold bg-clip-text bg-gradient-to-tr from-green-400 to-green-600 text-transparent">AI</span></h2>
                     <p className="text-muted-foreground text-md md:max-w-lg">Easily manage your business, automate responses, and engage customers—all within WhatsApp.</p>
                 </article>
 
                 {/* Links */}
-                <article className="flex gap-2 items-center">
+                <article className="flex gap-2 items-center justify-center md:justify-start">
                     {
                         [{ name: "Terms of Service", link: "terms-of-service" }, { name: "Pricing", link: "pricing" }, { name: "Contact Us", link: "contact-us" }].map((item: { name: string, link: string }, index: number) => (
                             <Link key={index} className="text-md text-green-600 font-semibold" href={item.link}>
@@ -30,7 +30,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
                 </article>
             </article>
 
-            <Card className="w-fit rounded-xl shadow-md shadow-green-200 border border-green-400 absolute right-4 bottom-10 top-10 px-4 py-8">
+            <Card className="w-fit rounded-xl shadow-md shadow-green-200 border border-green-400 mx-auto md:mx-0 md:absolute right-4 bottom-10 top-10 px-4 py-8">
                 <CardContent>
                     {children}
                 </CardContent>
