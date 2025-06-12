7cb515983c59bc07f1ae63e36c56e86879ded7e3
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-06-12 09:58:41 +0200

    feat: implement session management and routing for user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+"use client";
+import { authClient } from "@/lib/auth-client"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default Home
+export default function Home() {
+  const { data, isPending, error } = authClient.useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isPending) {
+      if (data) {
+        router.push("/dashboard");
+      } else {
+        router.push("/auth/signin");
+      }
+    }
+  }, [data, isPending, router]);
+
+  if (isPending) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Auth error</div>;
+  }
+
+  return null;
+}
