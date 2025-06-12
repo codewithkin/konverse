b28e81727692ffbf2052ea78d8874e53febc0377
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-06-12 10:04:19 +0200

    feat: add layout and page components for user authentication (sign in and sign up)

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Sign in or sign up to Konverse"
+}
+
+function AuthLayout() {
+    return (
+        <main>
+            <h2>Hi there !</h2>
+        </main>
+    )
+}
+
+export default AuthLayout
