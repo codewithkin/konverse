2299a6a92fbd43311d88f6106525f9ac785c0a7b
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-06-12 09:52:46 +0200

    feat: implement authentication client and API route for user authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,18 @@
 import prisma from "@/helpers/prisma";
-import {betterAuth} from "better-auth";
-import {prismaAdapter} from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
